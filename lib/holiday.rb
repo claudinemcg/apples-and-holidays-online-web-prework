81edcbe86b9ce7e7e71d7ed0d018d36ca52e685f
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-10 09:40:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,8 +35,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].collect do |key, value|
-    
-
+    items
+  end
+  array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
